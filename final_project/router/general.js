17428ad5fdd959b2d6fd476ffdd97e461ad7f7fb
alexeyyants <ayayants@googlemail.com>
2025-07-20 00:28:21 +0200

    implement general endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,32 +39,57 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books}, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  // Check if the book with the given ISBN exists
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  // Filter books by author
+  let books_by_author = Object.values(books).filter((book) => book.author === author);
+  // Check if any books by the author exist
+  if (books_by_author.length > 0) {
+    return res.status(200).json(books_by_author);
+  } else {
+    return res.status(404).json({message: "No books found by author " + author});
+  }
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  // Filter books by title
+  let books_by_title = Object.values(books).filter((book) => book.title === title);
+  // Check if any books with the title exist
+  if (books_by_title.length > 0) {
+    return res.status(200).json(books_by_title);
+  } else {
+    return res.status(404).json({message: "No books found with title " + title});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  // Check if the book with the given ISBN exists
+  if (books[isbn]) {
+    // Return the reviews of the book
+    return res.status(200).json(books[isbn].reviews);
+  } else {
+    return res.status(404).json({message: "Book with ISBN " + {isbn} + " not found"});
+  }
 });
 
 module.exports.general = public_users;
